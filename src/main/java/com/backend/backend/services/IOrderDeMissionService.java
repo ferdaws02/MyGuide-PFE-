3d6c2853e3a9378731830faf0756efb70ab45166
takwachtioui <takwa.chtiwi1995@gmail.com>
2023-10-19 15:24:18 +0200

    last commit

--- src/main/java/com/backend/backend/services/IOrderDeMissionService.java
+++ src/main/java/com/backend/backend/services/IOrderDeMissionService.java
@@ -1,18 +1,18 @@
 package com.backend.backend.services;
 
-
+import com.backend.backend.entities.Conge;
 import com.backend.backend.entities.DTOs.OrdreMissionDTO;
+import com.backend.backend.entities.EtatConge;
 import com.backend.backend.entities.OrdreDeMission;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-
-import org.springframework.http.ResponseEntity;
 
 public interface IOrderDeMissionService {
     public ResponseEntity<String> AjoutOdm(OrdreMissionDTO odm);
     public ResponseEntity<String> MettreAJourODM(OrdreMissionDTO odm);
    // public ResponseEntity<String> MettreAJourStatusCo(Long id, EtatConge Eco);
-    public List<OrdreMissionDTO> getAllODM() ;
+   List<OrdreMissionDTO> getAllODM();
     public void statusAnnuler(OrdreMissionDTO odm);
-    public ResponseEntity<String> MAJStatusNDFetODM(Long id_odm, String newEtat); 
+    public ResponseEntity<String> MAJStatusNDFetODM(Long id_odm, String newEtat);
 }
